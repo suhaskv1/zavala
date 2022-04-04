f8cce1f77f7f4e87fbdd50576f2adbeb321b65fb
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-04-04 21:06:56 +0530

    Update permutation.sv

--- src/permutation.sv
+++ src/permutation.sv
@@ -2,10 +2,6 @@
 // This file contains the permutation module of the SHA-3 implementation
 //
 
-//
-// This file contains the permutation module of the SHA-3 implementation
-//
-
 module permutation (
 	input logic [1599:0] in,
 	input logic [6:0] round_const,
